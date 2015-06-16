a71cdd55a9dad986d8a3b7aadc1b991e3834b12c
Justin Fong <justinf715@gmail.com>
2015-06-15 22:13:12 -0700

    Complete upvote functionality

--- client/app/questions/display.js
+++ client/app/questions/display.js
@@ -4,18 +4,25 @@ display.controller('DisplayController', function($scope, Questions) {
   
   var init = function() {
     $scope.getQuestions();
-    // setInterval($scope.getQuestions, 5000);
+    setInterval($scope.getQuestions, 10000);
   };
   
+
+  //For testing
   $scope.log = function(param) {
     console.log(param);
   };
   
   $scope.upvote = function(question){
-    Questions.upvote(question._id);
+    Questions.upvote(question._id)
+    .then(function(response) {
+      console.log(response);
+      $scope.getQuestions();
+    });
   };
 
   $scope.getQuestions = function() {
+    console.log('gotQuestions')
     Questions.getQuestions()
     .then(function(response){
       console.log(response.data);
--- server/upvote.js
+++ server/upvote.js
@@ -3,23 +3,13 @@ var router = express.Router();
 var db = require('./server.js')
 
 router.post('/', function(req, res) {
-  // console.log(req.body.id);
 
-  // db.Question.findOne({'_id': req.body.id}, function(err, question) {
-  //   console.log(question);
-  //   db.Question.update({'_id': req.body.id}, {$set: {'upvotes': question.upvotes + 1}});
-  //   res.end();
-  // })
-
-  // db.Question.update(
-  //    { '_id': req.body.id },
-  //    { $inc: { "upvotes": 1 } }
-  // )
 
   db.Question.findById(req.body.id, function(error, question) {
-    console.log(question);
+    // console.log(question);
     question.update({$inc: {'upvotes': 1}}, function(error, response) {
-      console.log(response);
+      // console.log(response);
+      res.end();
     });
   })
 
